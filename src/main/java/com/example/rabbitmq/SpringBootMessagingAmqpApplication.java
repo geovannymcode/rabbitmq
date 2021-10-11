0cd0f5b63e4902e66e4c47f6fa6e92a56c1f85e2
Geovanny Mendoza <geovanny0401@gmail.com>
2021-10-10 22:38:17 -0500

    Initial Commit

---
+++ src/main/java/com/example/rabbitmq/SpringBootMessagingAmqpApplication.java
@@ -0,0 +1,13 @@
+package com.example.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMessagingAmqpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMessagingAmqpApplication.class, args);
+	}
+
+}
